f5960e640a6ce558e9690d96ec4a8378d87badc2
rohit <rohit1113.cse19@chitkara.edu.in>
2022-09-01 13:50:59 +0530

    boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppRoute from "./app/index";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <AppRoute></AppRoute>
+    </Provider>
   </React.StrictMode>
 );
